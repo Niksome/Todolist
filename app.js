c43c1f9e29defe71df8e3b83d863dd52b4bffe16
Niksome <66631304+Niksome@users.noreply.github.com>
2023-05-09 19:43:57 +0200

    changings var to let

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [];
+let items = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", { kindOfDay: day, newListItems: items });
 });
